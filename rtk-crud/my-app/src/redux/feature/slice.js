e867eae52e18583364b90548d3815bc236eb8bb7
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-08-28 11:46:04 +0500

    toolkit

---
+++ rtk-crud/my-app/src/redux/feature/slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { value: 0 },
+  reducers: {
+    // add your non-async reducers here
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    }
+  },
+  extraReducers: {
+    // extraReducers handles asynchronous requests, which is our main focus.
+  }
+})
+// Action creators
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export default counterSlice.reducer;    
